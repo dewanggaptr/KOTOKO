6cf008f07a8104df734be2da0a53eb37098da613
dewanggaptr <dewanggajr30@gmail.com>
2023-03-15 10:12:52 +0700

    adding transactions controller

---
+++ app/controllers/api/transactions_controller.rb
@@ -0,0 +1,35 @@
+class Api::TransactionsController < ApplicationController
+  before_action :set_transaction, only: :destroy
+
+  def index
+    @transaction = Transaction.all
+
+    render json: @transaction.map { |transaction| transaction.new_attributes }
+  end
+
+  def create
+    @transaction = Transaction.new(transaction_params)
+
+    if @transaction.save
+      render json: @transaction.new_attributes, status: 201
+    else
+      render json: @transaction.errors, status: 422
+    end
+  end
+
+  def destroy
+    @transaction.destroy
+  end
+
+  private
+    def set_transaction
+      @transaction = Transaction.find_by_id(params[:id])
+      if @transaction.nil?
+        render json: { error: "Transaction not found" }, status: 404
+      end
+    end
+
+    def transaction_params
+      params.require(:transaction).permit(:cashier_id)
+    end
+end
