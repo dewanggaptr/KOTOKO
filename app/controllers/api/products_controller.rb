0f6e4de867baaca2dba33884f71d6a243e57cd4a
dewanggaptr <dewanggajr30@gmail.com>
2023-03-17 14:25:07 +0700

    add search filtering

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -2,7 +2,8 @@ class Api::ProductsController < ApplicationController
   before_action :set_product, only: [:show, :update, :destroy]
 
   def index
-    @products = Product.all
+    # @products = Product.all
+    @products = Product.search_product(params[:name])
 
     render json: @products.map { |product| product.new_attributes}
   end
@@ -34,6 +35,15 @@ class Api::ProductsController < ApplicationController
 
     render json: { success: "Success delete transaction" }, status: 200
   end
+
+  # def search
+  #   @parameter = params[:search].downcase
+  #   @result = Product.all.where("lower(name) LIKE :search", search: "#{@parameter}")
+
+  #   @result.each do |result|
+  #     result.name
+  #   end
+  # end
 
   private
   def set_product
